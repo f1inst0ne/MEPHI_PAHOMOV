9a2bc182238f3c24aee879191787cd5eddf6353f
f1inst0ne <Andrew.Pah@yandex.ru>
2022-10-08 10:30:53 +0300

    Started sin

---
+++ DZ_2/sin.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+long double new_sin(long double, int);
+
+
+int main(){
+	long double x = 0;
+	int n = 0;
+	printf("Enter x:");
+	scanf("%Lf", &x);
+	printf("Enter n:");
+	scanf("%d", &n);
+	return 0;
+}
+
+long double new_sin(long double x, int n){
+	
+}
