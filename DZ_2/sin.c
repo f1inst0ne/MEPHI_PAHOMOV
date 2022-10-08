640fdea78cb9f69f04a2b2aea25ebd324808969b
f1inst0ne <Andrew.Pah@yandex.ru>
2022-10-08 15:11:06 +0300

    Still doing SIN

--- DZ_2/sin.c
+++ DZ_2/sin.c
@@ -2,6 +2,9 @@
 #include<math.h>
 
 long double new_sin(long double, int);
+long double step(long double, int);
+int fact(int);
+int minus(int);
 
 
 int main(){
@@ -11,9 +14,57 @@ int main(){
 	scanf("%Lf", &x);
 	printf("Enter n:");
 	scanf("%d", &n);
+	while (x > 360){
+		x -= 360;
+	}
+	long double t_sin = sinl(x);
+	long double n_sin = new_sin(x, n);
+	printf("\nTrue sin:%.10Lf\n", t_sin);
+	printf("Your sin:%.10Lf\n", n_sin);
 	return 0;
 }
 
+
+/* --------------------------------------------- */
 long double new_sin(long double x, int n){
-	
+	long double s = 0;
+	long double chis = minus(n-1) * step(x, 2*n - 1);
+	int znam = fact(2*n - 1);
+	printf("----- s = %Lf, chis = %Lf, znam = %d, x = %Lf, n = %d\n", s, chis, znam, x, n);
+	while (n > 0){
+		s += chis / znam;
+		chis = chis / (x * x);
+		znam = znam / (n*(n-1));
+		n -= 2;
+	        printf("----- s = %Lf, chis = %Lf, znam = %d, x = %Lf, n = %d\n", s, chis, znam, x, n);
+
+	}
+	return s;
+}
+
+
+long double step(long double x, int n){
+	long double s = 1;
+	for (int i = 1; i < n+1; i++){
+		s *= x;
+	}
+	return s;
+}
+
+
+int fact(int n){
+	int f = 1;
+	for (int i = 1; i < n+1; i++){
+		f *= i;
+	}
+	return f;
+}
+
+
+int minus(int n){
+	int m = 1;
+	for (int i = 1; i < n+1; i++){
+		m *= -1;
+	}
+	return m;
 }
