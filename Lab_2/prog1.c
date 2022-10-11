b5c3d37d8c0c007b1483483d08c24497e599a578
f1inst0ne <Andrew.Pah@yandex.ru>
2022-10-11 21:20:39 +0300

    Lab 2 script nearly done

--- Lab_2/prog1.c
+++ Lab_2/prog1.c
@@ -1,3 +1,4 @@
+/*Лабораторную работу выполнил Пахомов Андреей Б22-554*/
 #include<stdio.h>
 #include<math.h>
 
@@ -7,22 +8,20 @@ long double func(int);
 int main(){
 	long double x = sqrt(2)/2;
 	int n = 0;/*Кол-во членов*/
-	printf("Введите кол-во членов:");
+	printf("\n\nВведите кол-во членов:");
 	scanf("%d", &n);
 	long double ans = func(n);
 	printf("Мой ответ:%.10Lf\n", ans);
-	printf("Правильный ответ:%.10Lf", x);
+	printf("Правильный ответ:%.10Lf\n\n\n", x);
 	return 0;
 }
 
 long double func(int n){
 	long double p = 1;
 	int s = -1;
-	printf("$ p = %.10Lf, s = %d, i = %d\n", p, s, 0);
-	for (int i = 1; i < n+1; i++){
-		p *= 1+(s/(2*i + 1));
+	for (long double i = 1; i < n+1; ++i){
+		p = p * (1+(s/(2*i + 1)));
 		s = -s;
-		printf("$ p = %.10Lf, s = %d, i = %d\n", p, s, i);
 
 	}
 	return p;
--- Lab_2/prog2.c
+++ Lab_2/prog2.c
@@ -1,7 +1,20 @@
+/*Лабораторную работу выполнил Пахомов Андреей Б22-554*/
 #include<stdio.h>
 #include<math.h>
 
+long double func(long double);
+
+
 int main(){
-	
+	long double n = 0;
+	printf("\n\nВведите точность:");
+	scanf("%Lf", &n);
+	long double ans = func(n);
+	long double t_ans = sqrt(2)/2;
+	printf("--------------------\nМой ответ:%.10Lf\nПравильный ответ:%.10Lf\n\n\n", ans, t_ans);
 	return 0;
+}
+
+long double func(long double n){
+	return 1231231;
 }
