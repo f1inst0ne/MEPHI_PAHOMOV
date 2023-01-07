86e6270db854a7216c59fbbb9f5ab5ab30174186
f1inst0ne <Andrew.Pah@yandex.ru>
2023-01-07 14:44:59 +0300

    prog1 Lab4 Done

--- Lab_4/prog1/task.c
+++ Lab_4/prog1/task.c
@@ -2,28 +2,40 @@
 #include<stdlib.h>
 #include<string.h>
 
-void task(char *str){
-	if ((str[0] == ' ') || (str[strlen(str)-1] == ' ')){
+void task(char *string){
+	if ((string[0] == ' ') || (string[strlen(string)-1] == ' ')){
 		printf("Ошибка!!!\nВведена строка оканчивающаяся на пробел или начинающаяся с него...\n");
 		return;
 	}
-	
+	char *str = (char*)calloc(128, sizeof(char));
+	strcpy(str, string);
 	char last_letter = str[strlen(str)-1];
-	char sep[]=" ";
 	char *word;
-	char *result = (char*)malloc(81*sizeof(char));
-	word = strtok(str, sep);
-	int word_len = strlen(word);
-	int result_len = 0;
+	char *result = (char*)calloc(128, sizeof(char));
+	char *istr = (char*)malloc(128*sizeof(char));
+	word = strtok(str, " ");
 	while (word != NULL){
-		if (word[0] == last_letter){
-			result = (char*)realloc(result, (word_len + result_len +1)* sizeof(char));
-			strcat(result, word);
-			strcat(result , " ");
+		strcpy(istr, word);
+		if (istr[0] == last_letter){
+			strcat(result, istr);
+			strcat(result, " ");
 		}
-		word  = strtok(NULL, sep);
-		word_len = strlen(word);
-		result_len = strlen(result) + word_len + 1;
+		word  = strtok(NULL, " ");
+	}
+	free(str);
+	str = (char*)calloc(128, sizeof(char));
+	strcpy(str,string);
+	word = strtok(str, " ");
+	while (word != NULL){
+		strcpy(istr, word);
+		if (istr[0] != last_letter){
+			strcat(result, istr);
+			strcat(result, " ");
+		}
+		word  = strtok(NULL, " ");
 	}
 	printf("%s\n", result);
+	free(str);
+	free(result);
+	free(istr);
 }
