89c927fd1d3702a746bec5151c8123e34392d22a
f1inst0ne <Andrew.Pah@yandex.ru>
2023-01-18 11:36:01 +0300

    Conrinue Lab5

---
+++ Lab_5/lib.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct node
+{
+	char number[8];//Номер участка
+	int age; //Возраст
+	char name[81]; //Уж 80 символов на имя думаю хватит
+
+}node;
--- Lab_5/main.c
+++ Lab_5/main.c
@@ -2,13 +2,21 @@
 #include<stdlib.h>
 #include<string.h>
 
+
 int main(int argc, char **argv)
 {
+
+	//Разбираемся с флагами
+
+
+	//Пустой ввод
 	if (argc <= 1)
 	{
 		printf("\nПамятка для тех кто здесь впервые!!!\n\nЗапустите исполянемый файл с флагом --help для справки\n\nНапрмер:\n ./filename --help\n\n");
 		return 0;
 	}
+
+	//Флаг --help
 	if (!strcmp(argv[1], "--help"))
 	{
 		FILE *file = fopen("help.txt", "r");
@@ -20,11 +28,27 @@ int main(int argc, char **argv)
 		fclose(file);
 		return 0;
 	}
-	if (argc % 2 == 0)
+
+	//Если флаги были криво введены
+	if ((argc < 6) || (argc >= 7) )
 	{
 		printf("ERROR!!!\n");
 		return 0;
 	}
+
+
+	FILE *input = fopen(argv[1], "r");
+	char buf[255];
+	while (fgets(buf, 255, input) != NULL)
+    	{
+        	printf("%s", buf);
+    	}
+	fclose(input);
+
+
+
+
+
 
 
 	return 0; 
