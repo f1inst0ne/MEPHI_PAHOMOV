2de87b9824c7456327e72627d4ed8f0f87cb0dd4
f1inst0ne <Andrew.Pah@yandex.ru>
2023-02-06 19:25:59 +0300

    Начал Lab5 заново

--- Lab_5/main.c
+++ Lab_5/main.c
@@ -1,55 +1,67 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
 
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 
-	//Разбираемся с флагами
-
-
-	//Пустой ввод
-	if (argc <= 1)
+	//--help
+	if (argc > 1)
 	{
-		printf("\nПамятка для тех кто здесь впервые!!!\n\nЗапустите исполянемый файл с флагом --help для справки\n\nНапрмер:\n ./filename --help\n\n");
+		if (strcmp(argv[1], "--help") == 0)
+		{
+			char line[255];
+			FILE *file = fopen("help.txt", "r");
+
+			if (file == NULL)
+			{
+				printf("Ошибка!!!\nФайла со справкой нет в директоркии!!!\n");
+				return 0;
+			}
+			while(fgets(line, 255, file) != NULL)
+			{
+			printf("%s", line);
+			}
+
+			fclose(file);
+
+			return 0;
+		}
+	}
+
+	//Проверка на кол-во аргументов
+	if (argc > 6){
+		printf("Слишком много аргументов.\n\nДля справки перезапустите с флагом --help\n");
+		return 0;
+	}
+	if ((argc < 5) && (argc == 1))
+	{
+		printf("Слишком мало аргументов.\n\nДля справки перезапустите с флагом --help\n");
 		return 0;
 	}
 
-	//Флаг --help
-	if (!strcmp(argv[1], "--help"))
-	{
-		FILE *file = fopen("help.txt", "r");
-		char str[255];
-		while (fgets(str, 255, file) != NULL)
-    	{
-        	printf("%s", str);
-    	}
-		fclose(file);
-		return 0;
-	}
+	time_t timer1 = time(NULL);  //Начальный таймер 
+	/*--------------------------------------------------*/
 
-	//Если флаги были криво введены
-	if ((argc < 6) || (argc >= 7) )
+
+	FILE *file = fopen(argv[1], "r");//Открываем файл
+	char line[255];
+	while (fgest(line, 255, file) != NULL)
 	{
-		printf("ERROR!!!\n");
-		return 0;
+		printf("%s", line);
 	}
 
 
-	FILE *input = fopen(argv[1], "r");
-	char buf[255];
-	while (fgets(buf, 255, input) != NULL)
-    	{
-        	printf("%s", buf);
-    	}
-	fclose(input);
+
+	fclose(file); //Закрываем файл
+
+	/*--------------------------------------------------*/
+	time_t timer2 = time(NULL); //Конечный таймер
+
+	printf("%f\n", difftime(timer2, timer1));
 
 
 
-
-
-
-
-	return 0; 
+	return 0;
 }
